06148448a30c43bb1caba18a5c9871adcea4af47
Vicky Gonzalez Bonorino <vickygbonorino@gmail.com>
2024-05-14 10:09:03 -0300

    se agregan productos mas vendidos

--- js/script.js
+++ js/script.js
@@ -9,5 +9,28 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-// Servicios
+// Mas vendidos
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    const slides = document.querySelectorAll('.carousel-images img');
+    
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    
+    slides[slideIndex - 1].style.display = 'block';
+    setTimeout(showSlides, 5000); // Cambiar imagen cada 5 segundos
+}
+
+function moveSlide(n) {
+    slideIndex += n;
+    showSlides();
+}
 
